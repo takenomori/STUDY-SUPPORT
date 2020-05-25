e1e65ede39cf2fd6910cabc94dd39dade02b2929
takeru0922 <moriai0922takeru@gmail.com>
2020-05-25 08:48:54 +0000

    add:model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :textbooks
+  has_many :study_times
+  has_many :calenders
+  has_many :tset_formats
+  has_many :test_dates
+
+  with_options presence: true do |u|
+    u.validates :first_name
+    u.validates :last_name
+    u.validates :kana_first_name
+    u.validates :kana_last_name
+  end
+
 end
