a3dead1bcd7b0f69628e624c21741920072d8f8c
takeru0922 <moriai0922takeru@gmail.com>
2020-05-28 03:34:24 +0000

    add:仮作成まで

--- app/controllers/textbooks_controller.rb
+++ app/controllers/textbooks_controller.rb
@@ -8,17 +8,43 @@ class TextbooksController < ApplicationController
   end
 
   def create
+    @textbook = Textbook.new(textbook_params)
+    @textbook.user_id = current_user.id
+    if @textbook.save
+      redirect_to textbooks_path(current_user.id), success: "登録されました"
+    else
+      @textbook = Textbook.new
+      render action: :new, danger: '登録に失敗しました'
+    end
   end
 
   def show
+    @textbook = Textbook.find(params[:id])
   end
 
   def edit
+    @textbook = Textbook.find(params[:id])
   end
 
   def update
+    @textbook = Textbook.find(params[:id])
+    if @textbook.update(textbook_params)
+      redirect_to textbook_path(@textbook.id), success: "更新されました"
+    else
+      render action: :edit, danger: '更新に失敗しました'
+    end
   end
 
   def destroy
+    @textbook = Textbook.find(params[:id])
+    @textbook.destroy
+    redirect_to textbooks_url
   end
+
+  private
+
+  def textbook_params
+    params.require(:textbook).permit(:user_id, :textbook_tag_id, :title, :image, :status, :note)
+  end
+
 end
