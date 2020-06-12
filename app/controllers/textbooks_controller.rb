82ee0b550961a867279ccd66187ba222b7cb11b9
takeru0922 <moriai0922takeru@gmail.com>
2020-06-12 17:01:21 +0900

    users,textbooksコントローラー作成

---
+++ app/controllers/textbooks_controller.rb
@@ -0,0 +1,13 @@
+class TextbooksController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
---
+++ test/controllers/textbooks_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class TextbooksControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get textbooks_index_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get textbooks_new_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get textbooks_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get textbooks_edit_url
+    assert_response :success
+  end
+
+end
