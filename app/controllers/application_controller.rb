ac15e6b9b1148bf56a0a438d8f0782a6977ef910
takeru0922 <moriai0922takeru@gmail.com>
2020-06-12 19:26:41 +0900

    deviseのコントローラー記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-        
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     add_flash_types :success, :info, :warning, :danger
 
     protected
+    
     #deviseのストロングパラメータ記述
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :kana_first_name, :kana_last_name])
