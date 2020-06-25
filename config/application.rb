4a70bc6e913dcb1770215333b5228ca5bf85f058
takeru0922 <moriai0922takeru@gmail.com>
2020-06-25 22:20:47 +0900

    [add]日本語化

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module AppName
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+        # デフォルトのlocaleを日本語(:ja)にする
+        config.i18n.default_locale = :ja
   end
 end
