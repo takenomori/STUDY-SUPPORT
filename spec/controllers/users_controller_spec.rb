7e96a8aacbcbbcdf0d3193ccd5afce9ff5e16c08
takeru0922 <moriai0922takeru@gmail.com>
2020-07-05 23:46:53 +0900

    [update]テスト記載

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,12 +2,15 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-    it "名前とメールアドレスとパスワードがあれば登録できる"
+    it "名前とメールアドレスとパスワードがあれば登録できる" do
+      expect(FactoryBot.create(:user)).to be_valid
+    end
+
+    it "名前がなければ登録できない" do
+    end
   
-    it "名前がなければ登録できない"
-  
-    it "メールアドレスがなければ登録できない"
-  
+    it "メールアドレスがなければ登録できない" do
+  end
     it "メールアドレスが重複していたら登録できない"
   
     it "パスワードがなければ登録できない"
