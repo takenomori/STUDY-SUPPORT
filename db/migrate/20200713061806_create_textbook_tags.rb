18bcbbc74766c546449cc34f567f93825499e238
takeru0922 <moriai0922takeru@gmail.com>
2020-07-14 14:34:02 +0900

    [delete]textbooktagsを一旦削除

--- db/migrate/20200713061806_create_textbook_tags.rb
+++
@@ -1,8 +0,0 @@
-class CreateTextbookTags < ActiveRecord::Migration[5.2]
-  def change
-    create_table :textbook_tags do |t|
-
-      t.timestamps
-    end
-  end
-end
---
+++ db/migrate/20200714053122_drop_textbook_tag.rb
@@ -0,0 +1,5 @@
+class DropTextbookTag < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :textbook_tags
+  end
+end
