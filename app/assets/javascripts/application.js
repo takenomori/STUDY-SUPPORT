de2dee4fc5d395bd50ea245749364003ffdb30f8
takeru0922 <moriai0922takeru@gmail.com>
2020-07-21 08:23:08 +0900

    [update]calenderサイズ変更

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,56 +21,54 @@
 //= require fullcalendar
 
 $(function () {
-    // 画面遷移を検知
-    $(document).on('turbolinks:load', function () {
-        // lengthを呼び出すことで、#calendarが存在していた場合はtrueの処理がされ、無い場合はnillを返す
-        if ($('#calendar').length) {
-            function eventCalendar() {
-                return $('#calendar').fullCalendar({
-                });
-            };
-            function clearCalendar() {
-                $('#calendar').html('');
-            };
+  // 画面遷移を検知
+  $(document).on("turbolinks:load", function () {
+    // lengthを呼び出すことで、#calendarが存在していた場合はtrueの処理がされ、無い場合はnillを返す
+    if ($("#calendar").length) {
+      function eventCalendar() {
+        return $("#calendar").fullCalendar({});
+      }
+      function clearCalendar() {
+        $("#calendar").html("");
+      }
 
-            $(document).on('turbolinks:load', function () {
-                eventCalendar();
-            });
-            $(document).on('turbolinks:before-cache', clearCalendar);
+      $(document).on("turbolinks:load", function () {
+        eventCalendar();
+      });
+      $(document).on("turbolinks:before-cache", clearCalendar);
 
-            $('#calendar').fullCalendar({
-                events: '/events.json',
-                //カレンダー上部を年月で表示させる
-                titleFormat: 'YYYY年 M月',
-                //曜日を日本語表示
-                dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
-                //ボタンのレイアウト
-                header: {
-                    left: '',
-                    center: 'title',
-                    right: 'today prev,next'
-                },
-                //終了時刻がないイベントの表示間隔
-                defaultTimedEventDuration: '03:00:00',
-                buttonText: {
-                    prev: '前',
-                    next: '次',
-                    prevYear: '前年',
-                    nextYear: '翌年',
-                    today: '今日',
-                    month: '月',
-                    week: '週',
-                    day: '日'
-                },
-                //イベントの時間表示を２４時間に
-                timeFormat: "HH:mm",
-                //イベントの色を変える
-                eventColor: '#63ceef',
-                //イベントの文字色を変える
-                eventTextColor: '#000000',
-                
-            });
-        }
-    });
+      $("#calendar").fullCalendar({
+        events: "/events.json",
+        //カレンダー上部を年月で表示させる
+        titleFormat: "YYYY年 M月",
+        //曜日を日本語表示
+        dayNamesShort: ["日", "月", "火", "水", "木", "金", "土"],
+        //ボタンのレイアウト
+        header: {
+          left: "",
+          center: "title",
+          right: "today prev,next",
+        },
+        //終了時刻がないイベントの表示間隔
+        defaultTimedEventDuration: "03:00:00",
+        buttonText: {
+          prev: "前",
+          next: "次",
+          prevYear: "前年",
+          nextYear: "翌年",
+          today: "今日",
+          month: "月",
+          week: "週",
+          day: "日",
+        },
+        height: 500,
+        //イベントの時間表示を２４時間に
+        timeFormat: "HH:mm",
+        //イベントの色を変える
+        eventColor: "#63ceef",
+        //イベントの文字色を変える
+        eventTextColor: "#000000",
+      });
+    }
+  });
 });
-
