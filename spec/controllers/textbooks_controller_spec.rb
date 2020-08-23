75703d09fb413f6dbe54e5b34355c6f8fe7561a3
takeru0922 <moriai0922takeru@gmail.com>
2020-08-23 23:27:50 +0900

    [update]テスト追記

---
+++ spec/controllers/textbooks_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Textbook, type: :model do
+#   it '名前first_nameがなければ登録できない' do
+#     expect(FactoryBot.build(:user, first_name: '')).to_not be_valid
+#   end
+#   it '名前last_nameがなければ登録できない' do
+#     expect(FactoryBot.build(:user, last_name: '')).to_not be_valid
+#   end
+#   it '名前kana_first_nameがなければ登録できない' do
+#     expect(FactoryBot.build(:user, kana_first_name: '')).to_not be_valid
+#   end
+#   it '名前kana_last_nameがなければ登録できない' do
+#     expect(FactoryBot.build(:user, kana_last_name: '')).to_not be_valid
+#   end
+
+#   it 'メールアドレスがなければ登録できない' do
+#     expect(FactoryBot.build(:user, email: '')).to_not be_valid
+#   end
+
+#   it 'パスワードがなければ登録できない' do
+#     expect(FactoryBot.build(:user, password: '')).to_not be_valid
+#   end
+end
